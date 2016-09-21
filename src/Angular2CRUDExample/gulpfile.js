a9a35711bb14f2e2180ea0c7f3fa057e24455ec2
Dancho Yordanov <danchosabev@gmail.com>
2016-09-21 14:02:25 +0300

    clear package.json and gulpfile.js

--- src/Angular2CRUDExample/gulpfile.js
+++ src/Angular2CRUDExample/gulpfile.js
@@ -1,7 +1,5 @@
 ﻿var gulp = require('gulp'),
-	gp_clean = require('gulp-clean'),
-	gp_concat = require('gulp-concat'),
-	gp_less = require('gulp-less'),
+	gp_clean = require('gulp-clean'),	
 	gp_sourcemaps = require('gulp-sourcemaps'),
 	gp_typescript = require('gulp-typescript'),
 	gp_uglify = require('gulp-uglify');
